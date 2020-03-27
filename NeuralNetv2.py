b6d4ae7dd762234ad2217fbb9b909754c23d5e82
Automatus <57252435+Automatus@users.noreply.github.com>
2020-03-27 16:55:47 +0100

    Update NeuralNetv2.py

--- NeuralNetv2.py
+++ NeuralNetv2.py
@@ -21,14 +21,14 @@ inputs = np.zeros((1, inputval))
 neurons = np.zeros((hl, nl))
 outputs = np.zeros((1, outputval))
 youtputs = np.zeros((1, outputval))
-spll = np.zeros(nl)  # spiking neurons in last layer that have a connection to the output that is being updated
+spll = np.zeros((1, nl))  # spiking neurons in last layer that have a connection to the output that is being updated
 
 while True:
     # Giving the Input
     print("Please give input")
     i = 0
-    for item in inputs:
-        inputs[i] = int(input("input for input " + str(i)))
+    for item in inputs[0, :]:
+        inputs[0, i] = int(input("input for input " + str(i)))
         i = +1
     print("Inputs:", inputs)
 
@@ -37,28 +37,28 @@ while True:
     i = -1  # = current layer
     while i <= hl + 1:
         if i == -1:
-            neurons[0, :] = (wi * inputs) > b
+            neurons[0, :] = (inputs[0, :].dot(wi)) > b
         if i == hl + 1:
-            outputs = neurons[hl-1, :] * wo
+            outputs = neurons[hl-1, :].dot(wo)
         else:
-            neurons[i, :] = (neurons[i-1, :] * w[i, :]) > b
+            neurons[i, :] = (neurons[i-1, :].dot(w[i, :])) > b
         i = +1
     print("calculated answer:", outputs)
 
     # Getting y/desired output
     print("Please give desired answers:")
     i = 0
-    for item in youtputs:
-        youtputs[i] = input("desired output for input" + str(i))
+    for item in youtputs[0, :]:
+        youtputs[0, i] = input("desired output for input" + str(i))
         i = +1
 
     # learning with backpropagation without derivatives
     print("Updating weigths")
     i = 0  # index for output
-    for item in outputs:  # iterating through outputs
+    for item in outputs[0, :]:  # iterating through outputs
         if 1 not in neurons[-1, :] and item:  # if no active neurons in last layer
             s = -2  # index for layer
-            for layer in neurons:  # iterating through layers
+            for layer in neurons[:, 0]:  # iterating through layers
                 stop = False
                 if not stop:
                     if 1 in neurons[s, :]:  # if active neuron present in layer s
@@ -70,13 +70,13 @@ while True:
                             t = +1
                 s = -1
         else:  # if active neurons in last layer are present
-            f_error = youtputs[i] - outputs[i]  # calculate error
+            f_error = youtputs[0, i] - outputs[0, i]  # calculate error
             j = -1  # index for layer of neurons
             k = 0  # index for neuron in layer j
             for neuron in neurons[j, :]:
                 if neuron:  # if neuron is firing/1/True
                     wo[i, k] = wo[i, k] + f_error * step
-                    spll[k] = True
+                    spll[0, k] = True
                 k = +1
             j = -1
             while j > -hl:  # while we are iterating neural layers
@@ -84,16 +84,16 @@ while True:
                 for neuron in neurons[j, :]:
                     if neuron:
                         z = 0
-                        for thing in spll:
-                            if spll[z]:
+                        for thing in spll[0, :]:
+                            if spll[0, z]:
                                 w[j, z, k] = w[j, z, k] + f_error * step
                     k = +1
             x = 0
-            for innn in inputs:
+            for innn in inputs[0, :]:
                 if innn:
                     q = 0
-                    for it in spll:
-                        if spll[q]:
+                    for it in spll[0, :]:
+                        if spll[0, q]:
                             wi[q, x] = wi[q, x] + f_error * step
                         q = +1
                 x = +1
