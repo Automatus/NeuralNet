250817dd8f7ae98d9b2787bc3d49eba5af83c0e9
Automatus <57252435+Automatus@users.noreply.github.com>
2020-04-13 17:13:28 +0200

    Update and rename NeuralNetv3.py to NeuralNetv2.1.py

--- NeuralNetv2.1.py
+++ NeuralNetv2.1.py
@@ -1,8 +1,7 @@
-# NeuralNet v.3
+# NeuralNet v.2.1
 # custom inputs, layers and outputs
 # neurons are spiking neurons
-# learning algorithm compares desired output with actual output
-# using numpy arrays
+# save and open files
 # learning step depends on number of neurons and is randomized to avoid perfect parallelisms
 
 import numpy as np
@@ -13,6 +12,8 @@ print("NeuralNet v3 by Automatus")
 
 b = 0.5  # treshold/bias
 step = 0.3  # learning rate
+minrand = 500  # minimal number for randomization
+maxrand = 1500  # maximal number for randomization
 
 while True:
     # Showing available projects
@@ -40,12 +41,30 @@ while True:
         youtputs = np.zeros((1, number_of_outputs))  # desired outputs
         spll = np.zeros((1, number_of_neurons_in_layer))  # SPiking neurons in Last investigated Layer that have a connection to the output that is being updated
 
-        file = open(os.listdir(os.path.join(os.getcwd(), "Nets", ProjectChoice)), "w")  # Creating new file
-        file.close()
-    else:
+        variables = np.array([b, step, minrand, maxrand, number_of_inputs, number_of_hidden_layers, number_of_neurons_in_layer, number_of_outputs])  # This array is only created to be able to save it easily
+        np.savez(os.path.join(os.getcwd(), "Nets", ProjectChoice), variables, wi, w, wo)
+    else:  # if existing file:
         ProjectChoice = networkslist[choice]
-        file = open(os.listdir(os.path.join(os.getcwd(), "Nets", ProjectChoice)), "r")  # Reading selected file
-        file.close()
+        npzfile = np.load(os.path.join(os.getcwd(), "Nets", ProjectChoice))  # open file
+
+        # Reading file
+        b = npzfile["arr_0"][0]
+        step = npzfile["arr_0"][1]
+        minrand = npzfile["arr_0"][2]
+        maxrand = npzfile["arr_0"][3]
+        number_of_inputs = npzfile["arr_0"][4]
+        number_of_hidden_layers = npzfile["arr_0"][5]
+        number_of_neurons_in_layer = npzfile["arr_0"][6]
+        number_of_outputs = npzfile["arr_0"][7]
+        wi = npzfile["arr_1"]
+        w = npzfile["arr_2"]
+        wo = npzfile["arr_3"]
+        
+        inputs = np.zeros((1, number_of_inputs))
+        neurons = np.zeros((number_of_hidden_layers, number_of_neurons_in_layer))
+        outputs = np.zeros((1, number_of_outputs))
+        youtputs = np.zeros((1, number_of_outputs))  # desired outputs
+        spll = np.zeros((1, number_of_neurons_in_layer))  # SPiking neurons in Last investigated Layer that have a connection to the output that is being updated
 
     # Giving the Input
     print("Please give input")
@@ -90,7 +109,7 @@ while True:
                             if bla:  # if input is active
                                 r = 0
                                 for thing in wi[t, :]:
-                                    wi[t, r] = wi[t, r] + step*random.randint(80, 120)/100  # strenghten weights/connections to neurons
+                                    wi[t, r] = wi[t, r] + step*random.randint(minrand, maxrand)/1000  # strenghten weights/connections to neurons
                                     r += 1
                             t += 1
                     elif 1 in neurons[j, :]:  # if active neuron present in layer s
@@ -100,7 +119,7 @@ while True:
                             if neuron:  # if neuron is active
                                 r = 0
                                 for thingy in w[j+1, :, t]:
-                                    w[j+1, r, t] = w[j+1, r, t] + step/number_of_neurons_in_layer*random.randint(80, 120)/100  # strenghten weights/connections in proportion to number of neurons
+                                    w[j+1, r, t] = w[j+1, r, t] + step/number_of_neurons_in_layer*random.randint(minrand, maxrand)/1000  # strenghten weights/connections in proportion to number of neurons
                                     r += 1
                             t += 1
                 j += -1
@@ -110,7 +129,7 @@ while True:
             k = 0  # index for neuron in layer j
             for neuron in neurons[j, :]:
                 if neuron:  # if neuron is firing/1/True
-                    wo[k, i] = wo[k, i] + f_error * step/number_of_neurons_in_layer*random.randint(80, 120)/100  # change weight strenght in proportion to error and number of neurons
+                    wo[k, i] = wo[k, i] + f_error * step/number_of_neurons_in_layer*random.randint(minrand, maxrand)/1000  # change weight strenght in proportion to error and number of neurons
                     spll[0, k] = True  # remember spiking neuron for next iterations
                 k += 1
             j = j - 1
@@ -121,7 +140,7 @@ while True:
                         z = 0
                         for thing in spll[0, :]:  # iterating through remembered spiking neurons
                             if spll[0, z]:  # if neuron spiked
-                                w[j+1, z, k] = w[j+1, z, k] + f_error * step/number_of_neurons_in_layer*random.randint(80, 120)/100  # change weight strenght in proportion to error and number of neurons
+                                w[j+1, z, k] = w[j+1, z, k] + f_error * step/number_of_neurons_in_layer*random.randint(minrand, maxrand)/1000  # change weight strenght in proportion to error and number of neurons
                             z += 1
                     k += 1
                 j += -1
@@ -131,7 +150,7 @@ while True:
                     q = 0
                     for it in spll[0, :]:  # iterating through remembered spiking neurons
                         if spll[0, q]:  # if neuron spiked
-                            wi[x, q] = wi[x, q] + f_error * step*random.randint(80, 120)/100  # change weight strenght in proportion to error
+                            wi[x, q] = wi[x, q] + f_error * step*random.randint(minrand, maxrand)/1000  # change weight strenght in proportion to error
                         q += 1
                 x += 1
         i += 1
